372165a932eba0aef00e1625a31261dca9bc0835
zero-x-two-a <cardsharing@secure-mail.biz>
2014-07-11 18:56:39 +0000

    Create install.sh
    This script installs all packages necessary to run OnionChat.

---
+++ src/install.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# Onion Chat by Gegenwind
+#   ____                               _           _ 
+#  / ___| ___  __ _  ___ _ ____      _(_)_ __   __| |
+# | |  _ / _ \/ _` |/ _ \ '_ \ \ /\ / / | '_ \ / _` |
+# | |_| |  __/ (_| |  __/ | | \ V  V /| | | | | (_| |
+#  \____|\___|\__, |\___|_| |_|\_/\_/ |_|_| |_|\__,_|
+#             |___/                                  
+#
+# Installs all necessary packages
+
+sudo apt-get -y install tor
+sudo apt-get -y install python
+sudo apt-get -y install python2.7
+sudo apt-get -y install python-pygame
+sudo apt-get -y install idle-python2.7
